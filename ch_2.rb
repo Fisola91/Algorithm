020a4a25b115a7d8e502c2f3753a6c10d5f5e186
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-10-03 03:33:41 +0200

    started working on chapter 2

---
+++ ch_2.rb
@@ -0,0 +1,21 @@
+## SELECTION SORT
+
+# A lot of Algorithms work if your data is sorted.
+
+
+
+# arrays and lists are  used to store multiple items.
+# There is no one right way to store items for every use case.
+
+
+
+## LINKED LISTS
+# Each item stores the address of the next item in the list.
+# As a result, you can't read the last item in a linked list.
+# You never have to move your items.
+# If there is space in memory, you have space for your linked list.
+# They are great for reading items one at a time.
+# Therefore, they are not good for reading items.
+
+## ARRAY
+# They are great for reading items.
