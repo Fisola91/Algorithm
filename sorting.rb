d88266fd1dd8219f5a4d1f9ac610cb79e6d276fd
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-12-31 08:45:17 +0100

    implementation of sorting algorithm using different method

--- sorting.rb
+++ sorting.rb
@@ -15,9 +15,11 @@ def bubble_sort(num)
 end
 
 # INSERTION SORT ALGORITHM O(n2)
+# Given array is divided into two part =>sorted and unsorted sublist
+# Take one value from the unsorted sublist and insert in appropiate position in the sorted sublist
 def insertion_sort(num)
-  i = 1
   length = num.length
+  i = 1
   while i < length
     temp = num[i]
     j = i - 1
@@ -31,4 +33,20 @@ def insertion_sort(num)
   num
 end
 
-p insertion_sort([3,1,5,2,6,7,4,6])
+def insertion_sort(num)
+  length = num.length
+  i = 1
+  (length-1).times do |len|
+    temp = num[i]
+    i += 1
+    while len >= 0 && num[len] > temp
+      num[len + 1] = num[len]
+      len -= 1
+    end
+    num[len+1] = temp
+  end
+  num
+end
+
+
+p insert([3,1,5,2,6,7,4,6])
